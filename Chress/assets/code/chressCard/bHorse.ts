e1aa737ebf13cc99d39ad9eb8f99d64febea9713
Cloud <dzclong@outlook.com>
2019-06-24 14:26:19 +0800

    增加马的路径

--- Chress/assets/code/chressCard/bHorse.ts
+++ Chress/assets/code/chressCard/bHorse.ts
@@ -13,6 +13,10 @@ const {ccclass, property} = cc._decorator;
 import {chressCard} from '../lib/defineGame';
 import Chress from '../lib/CardChress'
 
+
+import gm from "../gm"
+
+
 @ccclass
 export default class bHorse extends Chress {
 
@@ -25,6 +29,36 @@ export default class bHorse extends Chress {
     start () {
 
     }
+    /**
+     * 马共有八个位置，但是邻近有四个方向蹩马脚，只计算四个方向即可
+     */
+    getload():number[][]{
+        let temp = []
+        cc.log(gm.qipanNode)
+        if (this.positionx-1 >=0  ){
+            if(gm.qipanNode[this.positionx-1][this.positiony] ==null){
+                if(this.positionx -2 >=0 && this.positiony+1 >)
+            }
+        }
+        if (this.positionx+1 >=9  ){
+            if(gm.qipanNode[this.positionx+1][this.positiony] ==null){
+                
+            }
+        }
+        if (this.positiony +1 >=8  ){
+            if(gm.qipanNode[this.positionx][this.positiony+1] ==null){
+                
+            }
+        }
+        if (this.positiony -1 >=0  ){
+            if(gm.qipanNode[this.positionx][this.positiony-1] ==null){
+                
+            }
+        }
 
+        
+        
+        return temp
+    }
     // update (dt) {}
 }
